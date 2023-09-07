b10cc33deb331dea0f67f64e5c7cef93dfb02d0f
YashGumasana <yashgumasana@gmail.com>
2023-09-07 13:37:56 +0530

    change 10

--- src/controllers/user/youtube.ts
+++ src/controllers/user/youtube.ts
@@ -28,63 +28,63 @@ export const youtubeSignIn = async (req: Request, res: Response) => {
 
         console.log('filePath :>> ', filePath);
 
-        const installPythonDeps = spawn('pip', ['install', '-r', packageInstallPath]);
-        installPythonDeps.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
+        // const installPythonDeps = spawn('pip', ['install', '-r', packageInstallPath]);
+        // installPythonDeps.stdout.on('data', (data) => {
+        //     console.log(`stdout: ${data}`);
+        // });
+
+        // installPythonDeps.stderr.on('data', (data) => {
+        //     console.error(`stderr: ${data}`);
+        // });
+
+        // installPythonDeps.on('close', (code) => {
+        //     if (code === 0) {
+
+        //         // Now you can proceed to run your Python script
+        //     } else {
+        //         console.error(`Error: Python dependency installation failed with code ${code}`);
+        //     }
+        // });
+        console.log('Python dependencies installed successfully');
+        const pythonProcess = spawn('python', [filePath, userId]);
+
+        pythonProcess.stdout.on('data', (data) => {
+            console.log(`Python Output: ${data}`);
+            if (data.includes('error')) {
+                console.log('Error detected. Terminating Python process...');
+                pythonProcess.kill();
+            }
+
         });
 
-        installPythonDeps.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
+        pythonProcess.stderr.on('data', (data) => {
+            console.error(`Python Error: ${data}`);
+            pythonProcess.kill();
+
         });
 
-        installPythonDeps.on('close', (code) => {
+        pythonProcess.on('close', async (code) => {
+            console.log(`Python Process Exited with Code: ${code}`);
             if (code === 0) {
-                console.log('Python dependencies installed successfully');
-                const pythonProcess = spawn('python', [filePath, userId]);
+                // await userModel.findOneAndUpdate({ _id: userId }, { isYoutubeSignIn: true }, { new: true })
+                pythonProcess.kill();
 
-                pythonProcess.stdout.on('data', (data) => {
-                    console.log(`Python Output: ${data}`);
-                    if (data.includes('error')) {
-                        console.log('Error detected. Terminating Python process...');
-                        pythonProcess.kill();
-                    }
+                return res.status(200).json(new apiResponse(200, 'Login Successfull with Youtube', {}, {}))
+            }
+            else {
+                pythonProcess.kill();
 
-                });
-
-                pythonProcess.stderr.on('data', (data) => {
-                    console.error(`Python Error: ${data}`);
-                    pythonProcess.kill();
-
-                });
-
-                pythonProcess.on('close', async (code) => {
-                    console.log(`Python Process Exited with Code: ${code}`);
-                    if (code === 0) {
-                        // await userModel.findOneAndUpdate({ _id: userId }, { isYoutubeSignIn: true }, { new: true })
-                        pythonProcess.kill();
-
-                        return res.status(200).json(new apiResponse(200, 'Login Successfull with Youtube', {}, {}))
-                    }
-                    else {
-                        pythonProcess.kill();
-
-                        return res.status(500).json(new apiResponse(500, 'Internal Server Error', {}, {}))
-                    }
-                });
-
-                pythonProcess.on('error', (error) => {
-                    console.error(`Python Process Error: ${error}`);
-                    pythonProcess.kill(); // Terminate the Python process
-
-                    // Handle any other cleanup tasks if needed
-                    // For example, close any open connections, release resources, etc.
-                });
-                // Now you can proceed to run your Python script
-            } else {
-                console.error(`Error: Python dependency installation failed with code ${code}`);
+                return res.status(500).json(new apiResponse(500, 'Internal Server Error', {}, {}))
             }
         });
 
+        pythonProcess.on('error', (error) => {
+            console.error(`Python Process Error: ${error}`);
+            pythonProcess.kill(); // Terminate the Python process
+
+            // Handle any other cleanup tasks if needed
+            // For example, close any open connections, release resources, etc.
+        });
 
 
     } catch (error) {
